d77ea17f93d6026e4ac9221ef374069a34f2d03c
Marcos Vinicius <marcos.carini22@gmail.com>
2025-02-14 06:23:10 -0300

    build: update deployment configuration

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,9 @@
 // Fetch ou Axios
 import axios from "axios";
+import "dotenv/config";
 
-// const { NODE_ENV } = process.env;
-// const URL = "http://localhost:3001/api";
-const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
